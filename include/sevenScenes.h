98445f9f111f4a0ad993a766e1514b4d032bd921
cfiore-jpg <cameronfiore@anthonys-air-2.myfiosgateway.com>
2021-10-14 17:03:56 -0400

    clean up

--- include/sevenScenes.h
+++ include/sevenScenes.h
@@ -33,31 +33,9 @@ namespace sevenScenes
 
     vector<tuple<Eigen::Matrix3d, Eigen::Vector3d, Eigen::Matrix3d, Eigen::Vector3d, int>> pairSelection(const vector<pair<int, float>> &result, const vector<string> &listImage, const string &query, double loThresh, int maxNum, int method);
 
-    vector<tuple<Eigen::Matrix3d, Eigen::Vector3d, Eigen::Matrix3d, Eigen::Vector3d, int>> findXImagesWithYPoints(const vector<pair<int, float>> &result, const vector<string> &listImage, const string &query, int method, int X, int Y);
-
-    vector<Eigen::Vector3d> findAllHypotheses(const vector<tuple<Eigen::Vector3d, Eigen::Vector3d, int>> &processed);
-
     bool findEssentialMatrix(const string &db_image, const string &query_image, int method, Eigen::Matrix3d &R, Eigen::Vector3d &t, int& num_points);
 
-    void putInCorrectCluster(const cv::Point2d &q_point, const cv::Point2d &db_point, vector<pair<cv::Point2d, vector<pair<cv::Point2d, cv::Point2d>>>> &clusters);
-
-    bool converged(const vector<pair<cv::Point2d, vector<pair<cv::Point2d, cv::Point2d>>>> &old_clusters, const vector<pair<cv::Point2d, vector<pair<cv::Point2d, cv::Point2d>>>> &new_clusters);
-
-    vector<pair<vector<cv::Point2d>, vector<cv::Point2d>>> findClusters(const vector<cv::Point2d> &q_points, const vector<cv::Point2d> &db_points, int num_clusters, cv::Mat im);
-
-    double findFarthest(const vector<tuple<Eigen::Matrix3d, Eigen::Vector3d, Eigen::Matrix3d, Eigen::Vector3d>> &rel_images, const Eigen::Vector3d &avg);
-
-    Eigen::Vector3d findStandardDev(const vector<tuple<Eigen::Matrix3d, Eigen::Vector3d, Eigen::Matrix3d, Eigen::Vector3d>> &rel_images, const Eigen::Vector3d &avg);
-
-    bool withinXDevs(const Eigen::Vector3d &dev, const Eigen::Vector3d &avg, const Eigen::Vector3d &vec, int numDevs);
-
     double triangulateRays(const Eigen::Vector3d& ci, const Eigen::Vector3d& di, const Eigen::Vector3d& cj, const Eigen::Vector3d& dj, Eigen::Vector3d &intersect);
-
-    void findCorrectRiqs(const cv::Mat& E1, const Eigen::Matrix3d& R1, const cv::Mat& E2, const Eigen::Matrix3d& R2, Eigen::Matrix3d &Riq1, Eigen::Matrix3d &Riq2);
-
-    Eigen::Vector3d useRANSAC(const vector<tuple<Eigen::Matrix3d, Eigen::Vector3d, Eigen::Matrix3d, Eigen::Vector3d, int>> &rel_images, const double &inlier_thresh);
-
-    Eigen::Vector3d useRANSAC_v2(const vector<Eigen::Vector3d> &hypotheses, double inlier_thresh);
 }
 
 
--- src/sevenScenes.cpp
+++ src/sevenScenes.cpp
@@ -97,19 +97,6 @@ Eigen::Vector3d sevenScenes::getT(const string& image)
     }
 }
 
-double sevenScenes::getDistBetween(const Eigen::Vector3d &p1, const Eigen::Vector3d &p2)
-{
-    double x = p1(0) - p2(0);
-    double y = p1(1) - p2(1);
-    double z = p1(2) - p2(2);
-    return sqrt(pow(x, 2.0) + pow(y, 2.0) + pow(z, 2.0));
-}
-
-double sevenScenes::getAngleBetween(const Eigen::Vector3d &d1, const Eigen::Vector3d &d2)
-{
-    return acos(d1.dot(d2) / (d1.norm() * d2.norm())) * 180.0 / PI;
-}
-
 vector<tuple<string, string, vector<string>, vector<string>>> sevenScenes::createInfoVector()
 {
     vector<tuple<string, string, vector<string>, vector<string>>> info;
@@ -221,43 +208,22 @@ vector<tuple<Eigen::Matrix3d, Eigen::Vector3d, Eigen::Matrix3d, Eigen::Vector3d,
     return rel_images;
 }
 
-vector<tuple<Eigen::Matrix3d, Eigen::Vector3d, Eigen::Matrix3d, Eigen::Vector3d, int>> sevenScenes::findXImagesWithYPoints(const vector<pair<int, float>> &result, const vector<string> &listImage, const string &query, int method, int X, int Y)
+
+
+//Vector Measurements
+
+double sevenScenes::getDistBetween(const Eigen::Vector3d &p1, const Eigen::Vector3d &p2)
 {
-    vector<tuple<Eigen::Matrix3d, Eigen::Vector3d, Eigen::Matrix3d, Eigen::Vector3d, int>> toReturn;
-    int i = 0;
-    while(i < result.size() && toReturn.size() < X)
-    {
-        string image_i = listImage[result[i].first];
-        Eigen::Matrix3d Riq;
-        Eigen::Vector3d tiq;
-        int numPoints;
-        if(findEssentialMatrix(image_i, query, method, Riq, tiq, numPoints))
-        {
-            if(numPoints >= Y)
-            {
-                toReturn.push_back(tuple<Eigen::Matrix3d, Eigen::Vector3d, Eigen::Matrix3d, Eigen::Vector3d, int> (getR(image_i), getT(image_i), Riq, tiq, numPoints));
-            }
-        }
-        ++i;
-    }
-    return toReturn;
+    double x = p1(0) - p2(0);
+    double y = p1(1) - p2(1);
+    double z = p1(2) - p2(2);
+    return sqrt(pow(x, 2.0) + pow(y, 2.0) + pow(z, 2.0));
 }
 
-vector<Eigen::Vector3d> sevenScenes::findAllHypotheses(const vector<tuple<Eigen::Vector3d, Eigen::Vector3d, int>> &processed)
+double sevenScenes::getAngleBetween(const Eigen::Vector3d &d1, const Eigen::Vector3d &d2)
 {
-    vector<Eigen::Vector3d> toReturn;
-    for(int i = 0; i < processed.size(); ++i)
-    {
-        for(int j = i + 1; j < processed.size(); ++j)
-        {
-            Eigen::Vector3d h;
-            sevenScenes::triangulateRays(get<0>(processed[i]), get<1>(processed[i]), get<0>(processed[j]), get<1>(processed[j]), h);
-            toReturn.push_back(h);
-        }
-    }
-    return toReturn;
+    return acos(d1.dot(d2) / (d1.norm() * d2.norm())) * 180.0 / PI;
 }
-
 
 
 
@@ -480,158 +446,6 @@ bool sevenScenes::findEssentialMatrix(const string &db_image, const string &quer
     }
 }
 
-void sevenScenes::putInCorrectCluster(const cv::Point2d &q_point, const cv::Point2d &db_point, vector<pair<cv::Point2d, vector<pair<cv::Point2d, cv::Point2d>>>> &clusters)
-{
-    int closestIdx = 0;
-    double smallestDist = sqrt(pow((clusters[0].first.x - db_point.x), 2.0) + pow((clusters[0].first.y - db_point.y), 2.0));
-    for(int i = 1; i < clusters.size(); ++i)
-    {
-        double dist = sqrt(pow((clusters[i].first.x - db_point.x), 2.0) + pow((clusters[i].first.y - db_point.y), 2.0));
-        if (dist < smallestDist)
-        {
-            closestIdx = i;
-            smallestDist = dist;
-        }
-    }
-    clusters[closestIdx].second.push_back(pair<cv::Point2d, cv::Point2d> (q_point, db_point));
-}
-
-bool sevenScenes::converged(const vector<pair<cv::Point2d, vector<pair<cv::Point2d, cv::Point2d>>>> &old_clusters, const vector<pair<cv::Point2d, vector<pair<cv::Point2d, cv::Point2d>>>> &new_clusters)
-{
-    for(int i = 0; i < old_clusters.size(); ++i)
-    {
-        double dist = sqrt(pow((old_clusters[i].first.x - new_clusters[i].first.x), 2.0) + pow((old_clusters[i].first.y - new_clusters[i].first.y), 2.0));
-        if(dist >= 0.001)
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
-vector<pair<vector<cv::Point2d>, vector<cv::Point2d>>> sevenScenes::findClusters(const vector<cv::Point2d> &q_points, const vector<cv::Point2d> &db_points, int num_clusters, cv::Mat im)
-{
-
-    // Initialization
-    vector<bool> indexes (db_points.size(), true);
-    vector<pair<cv::Point2d, vector<pair<cv::Point2d, cv::Point2d>>>> old_clusters;
-    for(int i = 0; i < num_clusters; ++i)
-    {
-        cv::Point2d c;
-        vector<pair<cv::Point2d, cv::Point2d>> members;
-        while(true)
-        {
-            int idx = rand() % db_points.size();
-            if(indexes[idx])
-            {
-                indexes[idx] = false;
-                c = db_points[idx];
-                break;
-            }
-        }
-        old_clusters.push_back(pair<cv::Point2d, vector<pair<cv::Point2d, cv::Point2d>>> (c, members));
-    }
-
-    bool converged = false;
-    while(!converged)
-    {
-        //Place in clusters
-        for(int i = 0; i < q_points.size(); ++i)
-        {
-            putInCorrectCluster(q_points[i], db_points[i], old_clusters);
-        }
-
-
-        cv::Mat im2_clusters = im.clone();
-        for(int i = 0; i < old_clusters.size(); ++i)
-        {
-            int r = 30*i % 255;
-            int g = 60*i % 255;
-            int b = 90*i % 255;
-            cv::circle(im2_clusters, old_clusters[i].first, 8, cv::Scalar(r, g, b), -1);
-            for(const auto &point : old_clusters[i].second)
-            {
-                cv::circle(im2_clusters, point.second, 3, cv::Scalar(r, g, b), -1);
-            }
-        }
-        cv::imshow("DB Image Clusters", im2_clusters);
-        cv::waitKey();
-
-
-        vector<pair<cv::Point2d, vector<pair<cv::Point2d, cv::Point2d>>>> new_clusters;
-        //Update Cluster averages
-        for(const auto& p: old_clusters)
-        {
-            vector<pair<cv::Point2d, cv::Point2d>> new_members;
-            cv::Point2d total (0.0, 0.0);
-            if(p.second.empty())
-            {
-                new_clusters.push_back(pair<cv::Point2d, vector<pair<cv::Point2d, cv::Point2d>>> (p.first, new_members));
-                continue;
-            }
-            for(const auto &m: p.second)
-            {
-                total += m.second;
-            }
-            cv::Point2d new_center = total / double(p.second.size());
-            new_clusters.push_back(pair<cv::Point2d, vector<pair<cv::Point2d, cv::Point2d>>> (new_center, new_members));
-        }
-
-        converged = sevenScenes::converged(old_clusters, new_clusters);
-        if(!converged)
-        {
-            old_clusters = new_clusters;
-        }
-    }
-
-    vector<pair<vector<cv::Point2d>, vector<cv::Point2d>>> toReturn;
-    for(const auto& p: old_clusters)
-    {
-        vector<cv::Point2d> pts1, pts2;
-        for(const auto &point: p.second)
-        {
-            pts1.push_back(point.first);
-            pts2.push_back(point.second);
-        }
-        toReturn.push_back(pair<vector<cv::Point2d>, vector<cv::Point2d>> (pts1, pts2));
-    }
-
-    return toReturn;
-}
-
-Eigen::Vector3d sevenScenes::findStandardDev(const vector<tuple<Eigen::Matrix3d, Eigen::Vector3d, Eigen::Matrix3d, Eigen::Vector3d>> &rel_images, const Eigen::Vector3d &avg)
-{
-    Eigen::Vector3d sum;
-    sum << 0, 0, 0;
-    Eigen::Vector3d diff;
-    for(const auto & ri : rel_images)
-    {
-        diff = get<1>(ri) - avg;
-        diff(0) = diff(0) * diff(0);
-        diff(1) = diff(1) * diff(1);
-        diff(2) = diff(2) * diff(2);
-        sum = sum + diff;
-    }
-    sum = sum / (rel_images.size() - 1);
-    sum(0) = sqrt(sum(0));
-    sum(1) = sqrt(sum(1));
-    sum(2) = sqrt(sum(2));
-    return sum;
-}
-
-bool sevenScenes::withinXDevs(const Eigen::Vector3d &dev, const Eigen::Vector3d &avg, const Eigen::Vector3d &vec, int numDevs)
-{
-    double x = abs(avg(0) - vec(0));
-    double y = abs(avg(1) - vec(1));
-    double z = abs(avg(2) - vec(2));
-    Eigen::Vector3d d = numDevs * dev;
-    if(x <= d(0) && y <= d(1) && z <= d(2))
-    {
-        return true;
-    }
-    return false;
-}
-
 double sevenScenes::triangulateRays(const Eigen::Vector3d& ci, const Eigen::Vector3d& di, const Eigen::Vector3d& cj, const Eigen::Vector3d& dj, Eigen::Vector3d &intersect)
 {
     Eigen::Vector3d dq = di.cross(dj);
@@ -646,177 +460,10 @@ double sevenScenes::triangulateRays(const Eigen::Vector3d& ci, const Eigen::Vect
     return toReturn.norm();
 }
 
-void sevenScenes::findCorrectRiqs(const cv::Mat& E1, const Eigen::Matrix3d& R1, const cv::Mat& E2, const Eigen::Matrix3d& R2, Eigen::Matrix3d& Riq1, Eigen::Matrix3d& Riq2)
-{
-
-
-    cv::Mat Riq1A_cv, Riq1B_cv, Riq2A_cv, Riq2B_cv, t1, t2;
-    cv::decomposeEssentialMat(E1, Riq1A_cv, Riq1B_cv, t1);
-    cv::decomposeEssentialMat(E2, Riq2A_cv, Riq2B_cv, t2);
-
-    Eigen::Matrix3d Riq1A, Riq1B, Riq2A, Riq2B;
-    cv::cv2eigen(Riq1A_cv, Riq1A);
-    cv::cv2eigen(Riq1B_cv, Riq1B);
-    cv::cv2eigen(Riq2A_cv, Riq2A);
-    cv::cv2eigen(Riq2B_cv, Riq2B);
-
-    Eigen::Matrix3d G1A, G1B, G2A, G2B;
-    G1A = Riq1A * R1.transpose();
-    G1B = Riq1B * R1.transpose();
-    G2A = Riq2A * R2.transpose();
-    G2B = Riq2B * R2.transpose();
-
-    vector<pair<double, int>> angles;
-    angles.push_back(pair<double, int> (acos(((G1A * G2A.transpose()).trace() - 1)/2), 0));
-    angles.push_back(pair<double, int> (acos(((G1A * G2B.transpose()).trace() - 1)/2), 1));
-    angles.push_back(pair<double, int> (acos(((G1B * G2A.transpose()).trace() - 1)/2), 2));
-    angles.push_back(pair<double, int> (acos(((G1B * G2B.transpose()).trace() - 1)/2), 3));
-
-    sort(angles.begin(), angles.end(), [](const pair<double, int>& lhs, const pair<double, int>& rhs)
-    {
-        return lhs.first < rhs.first;
-    });
-
-    if(angles[0].second == 0)
-    {
-        Riq1 = Riq1A;
-        Riq2 = Riq2A;
-    } else if(angles[0].second == 1)
-    {
-        Riq1 = Riq1A;
-        Riq2 = Riq2B;
-    } else if(angles[0].second == 2)
-    {
-        Riq1 = Riq1B;
-        Riq2 = Riq2A;
-    } else if(angles[0].second == 3)
-    {
-        Riq1 = Riq1B;
-        Riq2 = Riq2B;
-    }
-}
-
-
 
 
 
 // Hypothesis
-
-Eigen::Vector3d sevenScenes::useRANSAC(const vector<tuple<Eigen::Matrix3d, Eigen::Vector3d, Eigen::Matrix3d, Eigen::Vector3d, int>> &rel_images, const double &inlier_thresh)
-{
-    vector<vector<tuple<Eigen::Vector3d, Eigen::Vector3d, int>>> models;
-//    Eigen::Vector3d dev = findStandardDev(rel_images, avg);
-    for(int i = 0; i < rel_images.size(); ++i)
-    {
-        for (int j = i + 1; j < rel_images.size(); ++j)
-        {
-            Eigen::Vector3d ci = get<1>(rel_images[i]);
-            Eigen::Vector3d di = get<0>(rel_images[i]) * get<2>(rel_images[i]).transpose() * get<3>(rel_images[i]);
-            Eigen::Vector3d cj = get<1>(rel_images[j]);
-            Eigen::Vector3d dj = get<0>(rel_images[j]) * get<2>(rel_images[j]).transpose() * get<3>(rel_images[j]);
-            Eigen::Vector3d h;
-            triangulateRays(ci, di, cj, dj, h);
-            vector<tuple<Eigen::Vector3d, Eigen::Vector3d, int>> model {tuple<Eigen::Vector3d, Eigen::Vector3d, int> (ci, di, get<4>(rel_images[i])), tuple<Eigen::Vector3d, Eigen::Vector3d, int> (cj, dj, get<4>(rel_images[j]))};
-            for (int k = 0; k < rel_images.size(); ++k)
-            {
-                if (k != i && k != j)
-                {
-                    Eigen::Vector3d t_pred = -get<2>(rel_images[k]) * get<0>(rel_images[k]).transpose() * (h - get<1>(rel_images[k]));
-                    double angle = getAngleBetween(t_pred, get<3>(rel_images[k]));
-
-                    if (angle <= inlier_thresh)
-                    {
-                        model.push_back(tuple<Eigen::Vector3d, Eigen::Vector3d, int> (get<1>(rel_images[k]), get<0>(rel_images[k]) * get<2>(rel_images[k]).transpose() * get<3>(rel_images[k]), get<4>(rel_images[k])));
-                    }
-                }
-            }
-            models.push_back(model);
-        }
-    }
-    if(models.empty())
-    {
-        return get<1>(rel_images[0]);
-    }
-    sort(models.begin(), models.end(), [](const auto &lhs, const auto &rhs)
-    {
-        return lhs.size() > rhs.size();
-    });
-    int s = models[0].size();
-    int idx = 0;
-    for(const auto& m : models)
-    {
-        if(m.size() < s)
-        {
-            break;
-        }
-        ++idx;
-    }
-    vector<vector<tuple<Eigen::Vector3d, Eigen::Vector3d, int>>> bestModels(idx);
-    copy(models.begin(), models.begin() + idx, bestModels.begin());
-    sort(bestModels.begin(), bestModels.end(), [](const auto &lhs, const auto &rhs)
-    {
-        Eigen::Vector3d g;
-        double p1 = get<2>(lhs[0]) + get<2>(lhs[1]);
-        double p2 = get<2>(rhs[0]) + get<2>(rhs[1]);
-        return p1 > p2;
-    });
-    vector<tuple<Eigen::Vector3d, Eigen::Vector3d, int>> toUse = bestModels[0];
-    cout << ", Inliers = " << toUse.size();
-
-    Eigen::Vector3d h;
-    triangulateRays(get<0>(toUse[0]), get<1>(toUse[0]), get<0>(toUse[1]), get<1>(toUse[1]), h);
-//    double total_points = 0;
-//    vector<pair<Eigen::Vector3d, int>> to_add;
-//    for (int y = 0; y < toUse.size(); ++y)
-//    {
-//        for (int z = y + 1; z < toUse.size(); ++z) {
-//            double cur_points = get<2>(toUse[y]) + get<2>(toUse[z]);
-//            total_points += cur_points;
-//            auto cy = get<0>(toUse[y]);
-//            auto dy = get<1>(toUse[y]);
-//            auto cz = get<0>(toUse[z]);
-//            auto dz = get<1>(toUse[z]);
-//            Eigen::Vector3d toAdd;
-//            triangulateRays(cy, dy, cz, dz, toAdd);
-//            to_add.push_back(pair<Eigen::Vector3d, int> (toAdd, cur_points));
-//        }
-//    }
-//
-//    Eigen::Vector3d h;
-//    h << 0, 0, 0;
-//    double test_total = 0;
-//    for (auto const &a : to_add)
-//    {
-//        test_total += (a.second/total_points);
-//        h += (a.second/total_points) * a.first;
-//    }
-    return h;
-}
-
-Eigen::Vector3d sevenScenes::useRANSAC_v2(const vector<Eigen::Vector3d> &hypotheses, double inlier_thresh)
-{
-    Eigen::Vector3d curBest;
-    int curMost = 0;
-    for(int i = 0; i < hypotheses.size(); ++i)
-    {
-        Eigen::Vector3d cur = hypotheses[i];
-        int curNum = 0;
-        for(int j = 0; j < hypotheses.size(); ++j)
-        {
-            if(i != j && getDistBetween(cur, hypotheses[j]) <= inlier_thresh)
-            {
-                ++curNum;
-            }
-        }
-        if (curNum > curMost)
-        {
-            curBest = cur;
-            curMost = curNum;
-        }
-    }
-    cout << ", Inliers = " << curMost;
-    return curBest;
-}
 
 
 
