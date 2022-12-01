bd89872749f610aede746f26229058f20c05770b
Cameron Fiore <cfiore@node1101.oscar.ccv.brown.edu>
2022-12-01 16:42:00 -0500

    histogram counts v1

--- main.cpp
+++ main.cpp
@@ -69,18 +69,24 @@ int main() {
     // vector<string> scenes = {"KingsCollege/", "OldHospital/", "ShopFacade/", "StMarysChurch/"};
     string dataset = "seven_scenes/";
     string relpose_file = "relpose_SP";
-    string error_file = "error_SP";
+    
 
     string ccv_dir = "/users/cfiore/data/cfiore/image_localization_project/data/"+dataset;
     string home_dir = "/Users/cameronfiore/C++/image_localization_project/data/";
     string dir = ccv_dir;
 
-    for (const auto & scene : scenes) {
-        ofstream error;
-        error.open(dir+scene+error_file+".txt");
+    ofstream matches_count_file;
+    matches_count_file.open(dir+"matches_count_file.txt");
 
-        double threshold = 5.;
-        double adj_threshold = 5.;
+    ofstream inliers_count_file;
+    inliers_count_file.open(dir+"inliers_count_file.txt");
+
+    map<int, int> inliers;
+    map<int, int> matches;
+
+    for (const auto & scene : scenes) {
+
+        double threshold = 10.;
 
         int start = 0;
         vector<string> queries = functions::getQueries(dir+"q.txt", scene);
@@ -108,282 +114,35 @@ int main() {
 
             Eigen::Vector3d c_q = -R_q.transpose() * T_q;
 
-    //        for (int i = 0; i < K; i++) {
-    //            Eigen::Matrix3d R_qi_real = R_q * R_is[i].transpose();
-    //            Eigen::Vector3d T_qi_real = T_q - R_qi_real * T_is[i];
-    //            T_qi_real.normalize();
-    //            double R_error = functions::rotationDifference(R_qi_real, R_qis[i]);
-    //            double T_error = functions::getAngleBetween(T_qi_real, T_qis[i]);
-    //            cout << R_error << ",  " << T_error << endl;
-    //            int check = 0;
-    //        }
-
-        // ALL K -------------------------------------------------------------------------------------------------------
-        int count = 0;
-        for (int i = 0; i < K - 1; i++) {
-            for (int j = i + 1; j < K; j++) {
-                count++;
-            }
-        }
-        std::thread threads_1[count];
-        count = 0;
-        auto * results = new vector<tuple<int,int,double,vector<int>>> ();
-        for (int i = 0; i < K - 1; i++) {
-            for (int j = i + 1; j < K; j++) {
-                threads_1[count] = thread(findInliers, threshold, i, j, &R_is, &T_is, &R_qis, &T_qis, results);
-                this_thread::sleep_for(std::chrono::microseconds (1));
-                count++;
-            }
-        }
-
-        // Sort by number of inliers
-        for (auto & th : threads_1) th.join();
-        sort(results->begin(), results->end(), [](const auto & a, const auto & b) {
-            return get<3>(a).size() > get<3>(b).size();
-        });
-
-        // Sort by lowest score
-        auto best_set = results->at(0);
-        int size = int(get<3>(best_set).size());
-        double best_score = get<2>(best_set);
-        int idx = 0;
-        while (true) {
-            try {
-                auto set = results->at(idx);
-                if (get<3>(set).size() != size) break;
-                if (get<2>(set) < best_score) {
-                    best_score = get<2>(set);
-                    best_set = set;
-                }
-            } catch (...) {
-                break;
-            }
-            idx++;
-        }
-        delete results;
-
-        vector<string> best_anchors;
-        vector<Eigen::Matrix3d> best_R_is, best_R_qis;
-        vector<Eigen::Vector3d> best_T_is, best_T_qis;
-        vector<vector<double>> best_K_is;
-        vector<vector<cv::Point2d>> best_inliers_q, best_inliers_i;
-        for (const auto & i : get<3>(best_set)) {
-            best_anchors.push_back(anchors[i]);
-            best_R_is.push_back(R_is[i]);
-            best_R_qis.push_back(R_qis[i]);
-            best_T_is.push_back(T_is[i]);
-            best_T_qis.push_back(T_qis[i]);
-            best_inliers_q.push_back(inliers_q[i]);
-            best_inliers_i.push_back(inliers_i[i]);
-            best_K_is.push_back(K_is[i]);
-        }
-
-        vector<Eigen::Matrix3d> rotations(best_R_is.size());
-        for (int i = 0; i < best_R_is.size(); i++) {
-            rotations[i] = best_R_qis[i] * best_R_is[i];
-        }
-
-        Eigen::Vector3d c_estimation = pose::c_q_closed_form(best_R_is, best_T_is, best_R_qis, best_T_qis);
-        Eigen::Matrix3d R_estimation = pose::R_q_average(rotations);
-
-        double c_error_estimation_all = functions::getDistBetween(c_q, c_estimation);
-        double R_error_estimation_all = functions::rotationDifference(R_q, R_estimation);
-
-        Eigen::Matrix3d R_adjustment = R_estimation;
-        Eigen::Vector3d T_adjustment = - R_estimation * c_estimation;
-
-        double avg_rep = pose::adjustHypothesis(best_R_is, best_T_is, best_K_is, K_q, best_inliers_q, best_inliers_i, adj_threshold, R_adjustment, T_adjustment);
-        Eigen::Vector3d c_adjustment = -R_adjustment.transpose() * T_adjustment;
-
-        double c_error_adjustment_all = functions::getDistBetween(c_q, c_adjustment);
-        double R_error_adjustment_all = functions::rotationDifference(R_q, R_adjustment);
-
-
-        line += " All_Pre_Adj " + to_string(R_error_estimation_all)
-                + " " + to_string(c_error_estimation_all)
-                + " All_Post_Adj " + to_string(R_error_adjustment_all)
-                + " " + to_string(c_error_adjustment_all);
-        //--------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            // Zhou Spacing ------------------------------------------------------------------------------------------------
-            K = int(anchors.size());
-            vector<Eigen::Vector3d> centers (K);
-            for(int i = 0; i < K; i++) {
-                centers[i] = - R_is[i].transpose() * T_is[i];
-            }
-        vector<int> spaced_indices = functions::optimizeSpacingZhou(centers, 0.05, 10., 20);
-        vector<string> anchors_zhou;
-        vector<Eigen::Matrix3d> R_is_zhou, R_qis_zhou;
-        vector<Eigen::Vector3d> T_is_zhou, T_qis_zhou;
-        vector<vector<double>> K_is_zhou;
-        vector<vector<cv::Point2d>> inliers_q_zhou, inliers_i_zhou;
-        for (const auto & i : spaced_indices) {
-            R_qis_zhou.push_back(R_qis[i]);
-            T_qis_zhou.push_back(T_qis[i]);
-            anchors_zhou.push_back(anchors[i]);
-            R_is_zhou.push_back(R_is[i]);
-            T_is_zhou.push_back(T_is[i]);
-            K_is_zhou.push_back(K_is[i]);
-            inliers_q_zhou.push_back(inliers_q[i]);
-            inliers_i_zhou.push_back(inliers_i[i]);
-        }
-
-        K = int(anchors_zhou.size());
-        count = 0;
-        for (int i = 0; i < K - 1; i++) {
-            for (int j = i + 1; j < K; j++) {
-                count++;
-            }
-        }
-        std::thread threads_2[count];
-        count = 0;
-        results = new vector<tuple<int,int,double,vector<int>>> ();
-        for (int i = 0; i < K - 1; i++) {
-            for (int j = i + 1; j < K; j++) {
-                threads_2[count] = thread(findInliers, threshold, i, j, &R_is_zhou, &T_is_zhou, &R_qis_zhou, &T_qis_zhou, results);
-                this_thread::sleep_for(std::chrono::microseconds (1));
-                count++;
-            }
-        }
-        // Sort by number of inliers
-        for (auto & th : threads_2) th.join();
-        sort(results->begin(), results->end(), [](const auto & a, const auto & b) {
-            return get<3>(a).size() > get<3>(b).size();
-        });
-        // Sort by lowest score
-        best_set = results->at(0);
-        size = int(get<3>(best_set).size());
-        best_score = get<2>(best_set);
-        idx = 0;
-        while (true) {
-            try {
-                auto set = results->at(idx);
-                if (get<3>(set).size() != size) break;
-                if (get<2>(set) < best_score) {
-                    best_score = get<2>(set);
-                    best_set = set;
-                }
-            } catch (...) {
-                break;
-            }
-            idx++;
-        }
-        delete results;
-
-        best_anchors.clear();
-        best_R_is.clear();
-        best_R_qis.clear();
-        best_T_is.clear();
-        best_T_qis.clear();
-        best_K_is.clear();
-        best_inliers_q.clear();
-        best_inliers_i.clear();
-        for (const auto & i : get<3>(best_set)) {
-            best_anchors.push_back(anchors_zhou[i]);
-            best_R_is.push_back(R_is_zhou[i]);
-            best_R_qis.push_back(R_qis_zhou[i]);
-            best_T_is.push_back(T_is_zhou[i]);
-            best_T_qis.push_back(T_qis_zhou[i]);
-            best_inliers_q.push_back(inliers_q_zhou[i]);
-            best_inliers_i.push_back(inliers_i_zhou[i]);
-            best_K_is.push_back(K_is_zhou[i]);
-        }
-
-        rotations.clear();
-        rotations.reserve(best_anchors.size());
-        for (int i = 0; i < best_R_is.size(); i++) {
-            rotations.emplace_back(best_R_qis[i] * best_R_is[i]);
-        }
-
-        c_estimation = pose::c_q_closed_form(best_R_is, best_T_is, best_R_qis, best_T_qis);
-        R_estimation = pose::R_q_average(rotations);
-
-        double c_error_estimation_zhou = functions::getDistBetween(c_q, c_estimation);
-        double R_error_estimation_zhou = functions::rotationDifference(R_q, R_estimation);
-
-        R_adjustment = R_estimation;
-        T_adjustment = - R_estimation * c_estimation;
-
-        avg_rep = pose::adjustHypothesis(best_R_is, best_T_is, best_K_is, K_q, best_inliers_q, best_inliers_i, adj_threshold, R_adjustment, T_adjustment);
-        c_adjustment = -R_adjustment.transpose() * T_adjustment;
-
-        double c_error_adjustment_zhou = functions::getDistBetween(c_q, c_adjustment);
-        double R_error_adjustment_zhou = functions::rotationDifference(R_q, R_adjustment);
-
-
-        line += " Zhou_Pre_Adj " + to_string(R_error_estimation_zhou)
-                + " " + to_string(c_error_estimation_zhou)
-                + " Zhou_Post_Adj " + to_string(R_error_adjustment_zhou)
-                + " " + to_string(c_error_adjustment_zhou);
-            //--------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-            // Our Spacing -------------------------------------------------------------------------------------------------
-            vector<int> our_indices = functions::optimizeSpacing(c_q, centers, 20, false);
-            vector<string> anchors_ours;
-            vector<Eigen::Matrix3d> R_is_ours, R_qis_ours;
-            vector<Eigen::Vector3d> T_is_ours, T_qis_ours;
-            vector<vector<double>> K_is_ours;
-            vector<vector<cv::Point2d>> inliers_q_ours, inliers_i_ours;
-            for (const auto & i : our_indices) {
-                R_qis_ours.push_back(R_qis[i]);
-                T_qis_ours.push_back(T_qis[i]);
-                anchors_ours.push_back(anchors[i]);
-                R_is_ours.push_back(R_is[i]);
-                T_is_ours.push_back(T_is[i]);
-                K_is_ours.push_back(K_is[i]);
-                inliers_q_ours.push_back(inliers_q[i]);
-                inliers_i_ours.push_back(inliers_i[i]);
-            }
-
-            K = int(anchors_ours.size());
-            count = 0;
+            // ALL K -------------------------------------------------------------------------------------------------------
+            int count = 0;
             for (int i = 0; i < K - 1; i++) {
                 for (int j = i + 1; j < K; j++) {
                     count++;
                 }
             }
-            std::thread threads_3[count];
+            std::thread threads_1[count];
             count = 0;
-            results = new vector<tuple<int,int,double,vector<int>>> ();
+            vector<tuple<int,int,double,vector<int>>> results;
             for (int i = 0; i < K - 1; i++) {
                 for (int j = i + 1; j < K; j++) {
-                    threads_3[count] = thread(findInliers, threshold, i, j, &R_is_ours, &T_is_ours, &R_qis_ours, &T_qis_ours, results);
+                    threads_1[count] = thread(findInliers, threshold, i, j, &R_is, &T_is, &R_qis, &T_qis, &results);
                     this_thread::sleep_for(std::chrono::microseconds (1));
                     count++;
                 }
             }
+
             // Sort by number of inliers
-            for (auto & th : threads_3) th.join();
+            for (auto & th : threads_1) th.join();
             sort(results->begin(), results->end(), [](const auto & a, const auto & b) {
                 return get<3>(a).size() > get<3>(b).size();
             });
+
             // Sort by lowest score
-            best_set = results->at(0);
-            size = int(get<3>(best_set).size());
-            best_score = get<2>(best_set);
-            idx = 0;
+            auto best_set = results->at(0);
+            int size = int(get<3>(best_set).size());
+            double best_score = get<2>(best_set);
+            int idx = 0;
             while (true) {
                 try {
                     auto set = results->at(idx);
@@ -397,57 +156,60 @@ int main() {
                 }
                 idx++;
             }
-            delete results;
 
-            best_anchors.clear();
-            best_R_is.clear();
-            best_R_qis.clear();
-            best_T_is.clear();
-            best_T_qis.clear();
-            best_K_is.clear();
-            best_inliers_q.clear();
-            best_inliers_i.clear();
+            vector<vector<cv::Point2d>> best_inliers_q, best_inliers_i;
             for (const auto & i : get<3>(best_set)) {
-                best_anchors.push_back(anchors_ours[i]);
-                best_R_is.push_back(R_is_ours[i]);
-                best_R_qis.push_back(R_qis_ours[i]);
-                best_T_is.push_back(T_is_ours[i]);
-                best_T_qis.push_back(T_qis_ours[i]);
-                best_inliers_q.push_back(inliers_q_ours[i]);
-                best_inliers_i.push_back(inliers_i_ours[i]);
-                best_K_is.push_back(K_is_ours[i]);
+                best_inliers_q.push_back(inliers_q[i]);
+                best_inliers_i.push_back(inliers_i[i]);
             }
 
-            rotations.clear();
-            for (int i = 0; i < best_R_is.size(); i++) {
-                rotations.emplace_back(best_R_qis[i] * best_R_is[i]);
+            map<cv::Point2d, int> matches_counts;
+            for (int i = 0; i < int(best_inliers_q.size); i++) {
+                auto points_q = best_inliers_q[i];
+                auto points_i = best_inliers_i[i];
+                for(int j = 0; j < int(points_q.size()); j++) {
+                    itr = matches_counts.find(points_q[j]);
+                    if (itr != m.end()) {
+                        itr->second += 1;
+                    } else {
+                        matches_counts.insert({points_q, 1});
+                    }
+                }
             }
 
-            c_estimation = pose::c_q_closed_form(best_R_is, best_T_is, best_R_qis, best_T_qis);
-            R_estimation = pose::R_q_average(rotations);
+            for (it = matches_counts.begin(); it != matches_counts.end(); it++) {
+                itr = matches.find(it->second);
+                if (itr != m.end()) {
+                        itr->second += 1;
+                } else {
+                    matches.insert({it->second, 1});
+                }
+            }
 
-            double c_error_estimation_ours = functions::getDistBetween(c_q, c_estimation);
-            double R_error_estimation_ours = functions::rotationDifference(R_q, R_estimation);
-
-            R_adjustment = R_estimation;
-            T_adjustment = - R_estimation * c_estimation;
-
-            avg_rep = pose::adjustHypothesis(best_R_is, best_T_is, best_K_is, K_q, best_inliers_q, best_inliers_i, adj_threshold, R_adjustment, T_adjustment);
-            c_adjustment = -R_adjustment.transpose() * T_adjustment;
-
-            double c_error_adjustment_ours = functions::getDistBetween(c_q, c_adjustment);
-            double R_error_adjustment_ours = functions::rotationDifference(R_q, R_adjustment);
-
-            line += " Ours_Pre_Adj " + to_string(R_error_estimation_ours)
-                    + " " + to_string(c_error_estimation_ours)
-                    + " Ours_Post_Adj " + to_string(R_error_adjustment_ours)
-                    + " " + to_string(c_error_adjustment_ours);
-            line += "\n";
-
-            cout << line << endl;
-            error << line;
+            itr = inliers.find(int(get<3>(best_set).size()));
+            if (itr != m.end()) {
+                    itr->second += 1;
+            } else {
+                inliers.insert({int(get<3>(best_set).size()), 1});
+            }
         }
-        error.close();
     }
+
+    for (it = matches.begin(); it != matches.end(); it++) {
+        int num = it->first;
+        int count = it->second;
+        matches_count_file << num << " " << count << endl;
+    }
+
+    for (it = inliers.begin(); it != inliers.end(); it++) {
+        int num = it->first;
+        int count = it->second;
+        inliers_count_file << num << " " << count << endl;
+    }
+
+
+    matches_file.close();
+    inliers_file.close();
+
     return 0;
 }
