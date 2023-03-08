b11e21792bc5ab74e82c466e55003359c10c3391
Cameron Fiore <cameron_fiore@brown.edu>
2023-03-08 01:12:48 -0500

    run good retrieved

--- main.cpp
+++ main.cpp
@@ -61,32 +61,31 @@ void findInliers (double thresh,
 
 int main() {
 
-    // vector<string> scenes = {"chess/", "fire/", "heads/", "office/", "pumpkin/", "redkitchen/", "stairs/"};
-    vector<string> scenes = {"chess/"};
+    vector<string> scenes = {"chess/", "fire/", "heads/", "office/", "pumpkin/", "redkitchen/", "stairs/"};
     string dataset = "seven_scenes/";
-    string error_file = "matches_histo_info";
+    string error_file = "good_recovered";
     int cutoff = -1;
     double thresh = 10;
     string relpose_file = "relpose_SP_SFM";
 
 //    vector<string> scenes = {"GreatCourt/", "KingsCollege/", "OldHospital/", "ShopFacade/", "StMarysChurch/"};
 //     string dataset = "cambridge/";
-// //     string error_file = "error_SP";
-//     string error_file = "matches_histo_info";
+//     string error_file = "error_SP";
+//    string error_file = "good_recovered";
 //     int cutoff = -1;
 //     double thresh = 5;
 //     string relpose_file = "relpose_SP";
 
     // vector<string> scenes = {"query/"};
     // string dataset = "aachen/";
-    // string error_file = "Aachen_eval_MultiLoc";
+//    string error_file = "good_recovered";
     // int cutoff = 3;
     // double thresh = 5;
     // string relpose_file = "relpose_SP";
 
 //    vector<string> scenes = {"query/"};
 //    string dataset = "robotcar/";
-//    string error_file = "Robotcar_eval_MultiLoc";
+//    string error_file = "good_recovered";
 //    int cutoff = 2;
 //    double thresh = 5;
 //    string relpose_file = "relpose_SP_v2";
@@ -95,15 +94,15 @@ int main() {
     string home_dir = "/Users/cameronfiore/C++/image_localization_project/data/" + dataset;
     string dir = ccv_dir;
 
+    ofstream error;
+    error.open(dir + error_file + ".txt");
     for (const auto &scene: scenes) {
-        ofstream error;
-        error.open(dir + scene + error_file + ".txt");
 
         int start = 0;
         vector<string> queries = functions::getQueries(dir + "q.txt", scene);
         for (int q = start; q < queries.size(); q++) {
 
-            cout << q + 1 << "/" << queries.size();
+            cout << q + 1 << "/" << queries.size() << endl;
             string query = queries[q];
 
             auto info = functions::parseRelposeFile(dir, query, relpose_file);
@@ -122,130 +121,13 @@ int main() {
             auto inliers_i = get<11>(info);
             int K = int(anchors.size());
 
-            Eigen::Matrix3d R_adjusted;
-            Eigen::Vector3d T_adjusted;
-            double c_error_est;
-            double r_error_est;
-            double c_error_adj;
-            double r_error_adj;
-            if (K == 0) {
-                R_adjusted << 1., 0., 0., 0., 1., 0., 0., 0., 1.;
-                T_adjusted << 0., 0., 0.;
-                cout << endl;
-            } else if (K == 1) {
-                R_adjusted = R_is[0];
-                T_adjusted = T_is[0];
-                cout << endl;
-            } else {
-
-                int s = 0;
-                for (int i = 0; i < K - 1; i++) {
-                    for (int j = i + 1; j < K; j++) {
-                        s++;
-                    }
-                }
-                int idx = 0;
-                vector<thread> threads(s);
-                vector<tuple<int, int, double, vector<int>>> results;
-                for (int i = 0; i < K - 1; i++) {
-                    for (int j = i + 1; j < K; j++) {
-                        threads[idx] = thread(findInliers, thresh, i, j, &R_is, &T_is, &R_qis, &T_qis,
-                                              &inliers_q, &results);
-                        idx++;
-                    }
-                }
-                for (auto &th: threads) {
-                    th.join();
-                }
-                sort(results.begin(), results.end(), [](const auto &a, const auto &b) {
-                    return get<3>(a).size() > get<3>(b).size();
-                });
-                vector<tuple<int, int, double, vector<int>>> results_trimmed;
-                for (int i = 0; i < results.size(); i++) {
-                    if (get<3>(results[i]).size() == get<3>(results[0]).size()) {
-                        results_trimmed.push_back(results[i]);
-                    } else {
-                        break;
-                    }
-                }
-                sort(results_trimmed.begin(), results_trimmed.end(), [](const auto &a, const auto &b) {
-                    return get<2>(a) > get<2>(b);
-                });
-                tuple<int, int, double, vector<int>> best_set = results_trimmed[0];
-
-                vector<string> best_anchors;
-                vector<Eigen::Matrix3d> best_R_is, best_R_qis;
-                vector<Eigen::Vector3d> best_T_is, best_T_qis;
-                vector<vector<double>> best_K_is;
-                vector<vector<cv::Point2d>> best_inliers_q, best_inliers_i;
-                for (const auto &i: get<3>(best_set)) {
-                    best_anchors.push_back(anchors[i]);
-                    best_R_is.push_back(R_is[i]);
-                    best_R_qis.push_back(R_qis[i]);
-                    best_T_is.push_back(T_is[i]);
-                    best_T_qis.push_back(T_qis[i]);
-                    best_inliers_q.push_back(inliers_q[i]);
-                    best_inliers_i.push_back(inliers_i[i]);
-                    best_K_is.push_back(K_is[i]);
-                }
-                vector<Eigen::Matrix3d> rotations(best_R_is.size());
-                vector<Eigen::Matrix3d> R_qis_real(best_R_is.size());
-                for (int i = 0; i < best_R_is.size(); i++) {
-                    rotations[i] = best_R_qis[i] * best_R_is[i];
-                }
-
-                vector<pair<pair<double, double>, vector<pair<int, int>>>> all_matches = functions::findSharedMatches(
-                        best_R_is, best_T_is, best_K_is, best_inliers_q, best_inliers_i);
-                error << int(best_R_is.size()) << " ";
-                for (const auto & p : all_matches) {
-                    error << int(p.second.size()) << " ";
-                }
-                error << endl;
-
-//                Eigen::Vector3d c_estimation = pose::c_q_closed_form(best_R_is, best_T_is, best_R_qis, best_T_qis);
-//                Eigen::Matrix3d R_estimation = pose::R_q_average(rotations);
-//                Eigen::Vector3d T_estimation = -R_estimation * c_estimation;
-//                c_error_est = functions::getDistBetween(c_q, c_estimation);
-//                r_error_est = functions::rotationDifference(R_q, R_estimation);
-
-//                cout << " " << best_R_is.size() << "/" << R_is.size();
-//
-//                R_adjusted = R_estimation;
-//                T_adjusted = -R_estimation * c_estimation;
-//                auto adj_points = pose::adjustHypothesis(best_R_is, best_T_is, best_K_is, K_q, best_inliers_q,
-//                                                         best_inliers_i,
-//                                                         R_adjusted, T_adjusted);
-//                Eigen::Vector3d c_adjusted = -R_adjusted.transpose() * T_adjusted;
-//                c_error_adj = functions::getDistBetween(c_q, c_adjusted);
-//                r_error_adj = functions::rotationDifference(R_q, R_adjusted);
-//
-//                cout << " " << adj_points.first.size() << endl;
-//            }
-//
-//            if (dataset == "aachen/" || dataset == "robotcar/") {
-//                Eigen::Quaterniond q_adj = Eigen::Quaterniond(R_adjusted);
-//                auto pos = query.find('/');
-//                string name = query;
-//                for (int c = 0; c < cutoff; c++) {
-//                    name = name.substr(pos + 1);
-//                    pos = name.find('/');
-//                }
-//                error << name << setprecision(17) << " " << q_adj.w() << " " << q_adj.x() << " "
-//                      << q_adj.y() << " " <<
-//                      q_adj.z() << " " << T_adjusted[0] << " " << T_adjusted[1] << " "
-//                      << T_adjusted[2] << endl;
-//            } else {
-//                string line;
-//                line += query + " All_Pre_Adj " + to_string(r_error_est)
-//                        + " " + to_string(c_error_est)
-//                        + " All_Post_Adj " + to_string(r_error_adj)
-//                        + " " + to_string(c_error_adj);
-//                error << line << endl;
-//                cout << line << endl;
-                cout << endl;
+            int num_good = 0;
+            for(const auto & points : inliers_q) {
+                if (points.size() >= 50) num_good++;
             }
+            error << num_good << endl;
         }
-    error.close();
     }
+    error.close();
     return 0;
 }
